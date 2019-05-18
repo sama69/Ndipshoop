4d9add44e9c2a4037299246ba72dba11dbc93dae
sama69 <48472313+sama69@users.noreply.github.com>
2019-05-18 13:26:31 +0700

    Update update.php

--- www/update.php
+++ www/update.php
@@ -2,11 +2,13 @@
  include "db.php";
  if(isset($_POST['update']))
  {
- $id=$_POST['id'];
- $title=$_POST['title'];
- $duration=$_POST['duration'];
- $price=$_POST['price'];
- $q=mysqli_query($con,"UPDATE `course_details` SET `title`='$title',`duration`='$duration',`price`='$price' where `id`='$id'");
+ $id_mhs=$_POST['id_mhs'];
+ $nama=$_POST['nama'];
+$nim=$_POST['nim'];
+ $progdi=$_POST['progdi'];
+ $no_telp=$_POST['no_telp'];
+  $email=$_POST['email'];
+ $q=mysqli_query($con,"UPDATE `dt_mhs` SET `id_mhs`='$id_mhs',`nama`='$nama',`nim`='$nim',`progdi`='$progdi',`no_telp`='$no_telp',`email`='$email' where `id_mhs`='$id_mhs'");
  if($q)
  echo "success";
  else
