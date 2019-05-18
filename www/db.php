b0f1dcc48c2636962c7fae19e76a943ec9e506a9
sama69 <48472313+sama69@users.noreply.github.com>
2019-05-18 13:23:33 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","root","root","phonegap_demo") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id7899380_sertifikasi","sertifikasi","id7899380_sertifikasi") or die ("could not connect database");
 ?>
