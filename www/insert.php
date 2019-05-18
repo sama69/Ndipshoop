bb4da2478ef26071892f110b6a1fa87bc097e665
sama69 <48472313+sama69@users.noreply.github.com>
2019-05-18 13:25:22 +0700

    Update insert.php

--- www/insert.php
+++ www/insert.php
@@ -2,11 +2,11 @@
  include "db.php";
  if(isset($_POST['insert']))
  {
- $title=$_POST['title'];
- $duration=$_POST['duration'];
- $price=$_POST['price'];
- $q=mysqli_query($con,"INSERT INTO `course_details` (`title`,`duration`,`price`) VALUES ('$title','$duration','$price')");
- if($q)
+ $nim=$_POST['id_mhs'];
+ $nama=$_POST['nama'];
+ $sertifikasi=$_POST['sertifikasi'];
+ $q=mysqli_query($con,"INSERT INTO `peserta` (`id_mhs`,`nama`,`sertifikasi`) VALUES ('$id_mhs','$nama','$sertifikasi')");
+ if($q)   
   echo "success";
  else
   echo "error";
