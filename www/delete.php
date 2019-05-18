82689b9f692e8f5f5f3e3b9cd9b52fef460e83c8
sama69 <48472313+sama69@users.noreply.github.com>
2019-05-18 13:24:03 +0700

    Update delete.php

--- www/delete.php
+++ www/delete.php
@@ -3,7 +3,7 @@
  if(isset($_GET['id']))
  {
  $id=$_GET['id'];
- $q=mysqli_query($con,"delete from `course_details` where `id`='$id'");
+ $q=mysqli_query($con,"delete from `dt_mhs` where `id_mhs`='$id_mhs'");
  if($q)
  echo "success";
  else
