9bf63b68f0d6bce21022e6cdf68f6653bbcb1f8b
sama69 <48472313+sama69@users.noreply.github.com>
2019-05-20 12:11:47 +0700

    Update update.php

--- www/update.php
+++ www/update.php
@@ -2,13 +2,13 @@
  include "db.php";
  if(isset($_POST['update']))
  {
- $id_mhs=$_POST['id_mhs'];
+ $nim=$_POST['nim'];
  $nama=$_POST['nama'];
-$nim=$_POST['nim'];
- $progdi=$_POST['progdi'];
- $no_telp=$_POST['no_telp'];
-  $email=$_POST['email'];
- $q=mysqli_query($con,"UPDATE `dt_mhs` SET `id_mhs`='$id_mhs',`nama`='$nama',`nim`='$nim',`progdi`='$progdi',`no_telp`='$no_telp',`email`='$email' where `id_mhs`='$id_mhs'");
+$progdi=$_POST['progdi'];
+ $sertif=$_POST['sertif'];
+ $email=$_POST['email'];
+  $nohp=$_POST['nohp'];
+ $q=mysqli_query($con,"UPDATE `data_mhs` SET `nim`='$nim',`nama`='$nama',`progdi`='$progdi',`sertif`='$sertif',`email`='$email',`nohp`='$nohp' where `nim`='$nim'");
  if($q)
  echo "success";
  else
