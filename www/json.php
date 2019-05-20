4cba1390715302aee2f943ad87662531b35c346d
sama69 <48472313+sama69@users.noreply.github.com>
2019-05-20 12:08:20 +0700

    Update json.php

--- www/json.php
+++ www/json.php
@@ -1,10 +1,9 @@
 <?php
 include "db.php";
 $data=array();
-$q=mysqli_query($con,"select * from `dt_mhs`");
+$q=mysqli_query($con,"select * from `data_mhs`");
 while ($row=mysqli_fetch_object($q)){
  $data[]=$row;
 }
 echo json_encode($data);
 ?>
-
